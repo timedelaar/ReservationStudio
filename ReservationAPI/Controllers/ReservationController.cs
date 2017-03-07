7581b62676176482a30985998da5d066cea94ff6
khp815 <13049887@student.hhs.nl>
2017-03-07 16:42:10 +0100

    Api/reservationController

--- ReservationAPI/Controllers/ReservationController.cs
+++ ReservationAPI/Controllers/ReservationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReservationAPI.Models;
 
 
 namespace ReservationAPI.Controllers
@@ -17,13 +18,15 @@ namespace ReservationAPI.Controllers
 			_DataContext = DataContext;
 		}
 
-		// GET: api/values
+        //Get a list of all reservations
+        //GET: api/values
 		[HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Reservation> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new List<Reservation>();
         }
 
+        // Get a single reservation by Id
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
@@ -31,18 +34,22 @@ namespace ReservationAPI.Controllers
             return "value";
         }
 
+        // Create a new reservation
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Reservation reservation)
         {
+            
         }
 
+        // Update the info of a reservation
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Reservation reservation)
         {
         }
 
+        // Delete a reservation by Id
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
